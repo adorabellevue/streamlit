d0c9dda44f3f1a5628c3fcf42a07881bc805be2a
adorabellevue <adorawu@cs.washington.edu>
2024-12-17 21:55:19 -0800

    update

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from PIL import Image
+import io
 
-st.write("hello world")
+st.title("My Website")
+myname = st.text_input('Your name')
+if myname:
+    st.header('your name is ' + myname)
+myimg = st.file_uploader("upload a photo", type=['jpg', 'webp'])
+if myimg:
+    img_data = myimg.read()
+    image = Image.open(io.BytesIO(img_data))
+    st.image(image, caption='上面是收到的照片', width=300)
